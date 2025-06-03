86fef249b9de4608b503acf5613d66fcad14f2fd
Edgar Jason Husin <edgarjasonhusin@gmail.com>
2025-06-03 22:46:46 +0700

    ubah icon aplikasi

--- GuiTileMapThing/Main.java
+++ GuiTileMapThing/Main.java
@@ -5,21 +5,24 @@ import javax.swing.*;
 
 public class Main {
     protected static final int screenWidth = 800;
-    protected static final int screenHeight = 800; 
+    protected static final int screenHeight = 800;
 
     public static void main(String argz[]) {
-        JFrame frame = new JFrame("pukemon");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(screenWidth, screenHeight);
-            frame.setMinimumSize(new Dimension(screenWidth, screenHeight));
-            frame.getContentPane().setBackground(Color.BLACK);
-            JPanel outerPanel = new JPanel(new GridBagLayout());
-            outerPanel.setBackground(Color.BLACK);
-             
-            GamePanel gamePanel = new GamePanel();
-            outerPanel.add(gamePanel);
-            frame.add(outerPanel, BorderLayout.CENTER);
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+        JFrame frame = new JFrame("Pokemon");
+        ImageIcon icon = new ImageIcon("TileGambar/iconApp.png"); 
+        frame.setIconImage(icon.getImage());
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(screenWidth, screenHeight);
+        frame.setMinimumSize(new Dimension(screenWidth, screenHeight));
+        frame.getContentPane().setBackground(Color.BLACK);
+        JPanel outerPanel = new JPanel(new GridBagLayout());
+        outerPanel.setBackground(Color.BLACK);
+
+        GamePanel gamePanel = new GamePanel();
+        outerPanel.add(gamePanel);
+        frame.add(outerPanel, BorderLayout.CENTER);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
